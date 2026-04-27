5479bd0b17915ad1a8cd8ef350c0e2dd535f24c0
Ahmed Samy <ahmedenany049@gmail.com>
2026-04-27 17:54:33 +0300

    fix env path

--- src/DB/connectionDB.ts
+++ src/DB/connectionDB.ts
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 
 const connectionDB=async()=>{
-    console.log("DB_URL:", process.env.DB_URL)
-
     mongoose.connect(process.env.DB_URL as unknown as string)
     .then(()=>{
         console.log("success to connect db.........");
